3dffdc2bd40d1c1ec8c17c9ca42399991f18f936
yaniszhao <361954576@qq.com>
2018-01-09 16:55:42 +0800

    modify 1014.c 1017.c

--- 1014.c
+++ 1014.c
@@ -4,6 +4,7 @@
 #define PEOMAX 1001
 #define WINMAX 20
 #define LINEMAX 11
+#define TIMEMAX 1e5
 
 #define DEBUG 0
 
@@ -188,9 +189,150 @@ void fun1()
 	}
 
 }
+//===============================================
+
+typedef struct stWindow {
+    int lineque[LINEMAX];
+    int linenum;
+    int head;
+    int tail;
+} Window;
+
+typedef struct stWaitQue {
+    int lineque[PEOMAX];
+    int linenum;
+    int head;
+    int tail;
+} WaitQue;
+
+int IsMin2(Window w[], int idx, int n)
+{
+    int i;
+    for (i=0; i<n; i++)
+        if (w[i].linenum<w[idx].linenum)
+            return 0;
+    return 1;
+}
+
+int CountWindowQueNum(Window w[], int n)
+{
+    int i, cnt=0;
+    for (i=0; i<n; i++)
+        cnt+=w[i].linenum;
+    return cnt;
+}
+
+void fun2() {
+    Window window[WINMAX];
+    WaitQue wait;
+    int sttime[PEOMAX], endtime[PEOMAX];
+    int n, m, k, q;
+    int mins[PEOMAX];
+    int qIdx[PEOMAX];
+    int i, j, cur, mintime;
+    char str[6];
+
+    wait.head=0, wait.tail=0, wait.linenum=0;
+    for (i=0; i<WINMAX; i++) {
+        window[i].head=window[i].tail=0;
+        window[i].linenum=0;
+    }
+
+    for (i=0; i<PEOMAX; i++) {
+        sttime[i]=endtime[i]=-1;
+    }
+
+    scanf("%d%d%d%d", &n,&m,&k,&q);
+    for (i=1; i<=k; i++) {
+        scanf("%d", &mins[i]);
+        wait.lineque[wait.tail++]=i;
+        wait.linenum++;
+    }
+
+    for (i=0; i<q; i++) {
+        scanf("%d", &qIdx[i]);
+    }
+
+    //first inque
+   /*for (j=0; j<n*m && j<k; j++) {
+       for (i=0; i<n; i++) {//windows
+           if (window[i].linenum<m && IsMin2(window, i, n)) {
+               window[i].lineque[window[i].tail]=wait.lineque[wait.head++];//inque
+               window[i].tail=(window[i].tail+1)%LINEMAX;
+               window[i].linenum++;
+               break;
+           }
+       }
+   }*/
+    for (j=0,i=0; j<n*m && j<k; j++, i=(i+1)%n) {
+        window[i].lineque[window[i].tail]=wait.lineque[wait.head];//inque
+        window[i].tail=(window[i].tail+1)%LINEMAX;
+        window[i].linenum++;
+        wait.head++;
+        wait.linenum--;
+    }
+
+    //first set time
+    for (i=0; i<n; i++) {//windows
+        if (window[i].linenum<1) continue;
+        cur=window[i].lineque[window[i].head];
+        if (sttime[cur]==-1)
+            sttime[cur]=0;
+    }
+
+    while (CountWindowQueNum(window, n) > 0) {
+        mintime=TIMEMAX;
+        for (i=0; i<n; i++) {//get mimtime
+            if (window[i].linenum<1) continue;
+            cur=window[i].lineque[window[i].head];
+            mintime=(mins[cur]+sttime[cur] < mintime)?(mins[cur]+sttime[cur]):mintime;
+        }
+
+        for (i=0; i<n; i++) {//outque
+            if (window[i].linenum<1) continue;
+            cur=window[i].lineque[window[i].head];
+            if (mins[cur]+sttime[cur] == mintime) {
+                window[i].head=(window[i].head+1)%LINEMAX;
+                window[i].linenum--;
+            }
+        }
+
+        //if (wait.linenum==0 || mintime>=9*60) continue;//不能放在这影响set time
+
+        for (i=0; i<n; i++) {//windows,inque
+            if (wait.linenum==0) break;//no people wait, or time full
+            if (window[i].linenum<m && IsMin2(window, i, n)) {
+                window[i].lineque[window[i].tail]=wait.lineque[wait.head];//inque
+                window[i].tail=(window[i].tail+1)%LINEMAX;
+                window[i].linenum++;
+                wait.head++;
+                wait.linenum--;
+            }
+        }
+
+        //set time
+        for (i=0; i<n; i++) {//windows
+            if (window[i].linenum<1) continue;
+            cur=window[i].lineque[window[i].head];
+            if (sttime[cur] == -1)
+                sttime[cur]=mintime;
+        }
+    }
+
+    for (i=0; i<q; i++) {
+        cur=qIdx[i];
+        if (sttime[cur]!=-1 && sttime[cur]<9*60) {
+            IntToStr(sttime[cur]+mins[cur], str);
+            printf("%s\n", str);
+        }
+        else
+            printf("Sorry\n");
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
-	fun1();
-
+   //fun1();
+   fun2();
 }
--- 1017.c
+++ 1017.c
@@ -4,6 +4,7 @@
 #define PEOMAX 10000
 #define WINMAX 100
 #define LINEMAX 1
+#define TIMEMAX 1e7
 
 #define DEBUG 0
 
@@ -141,8 +142,200 @@ void fun1()
 
 }
 
+//===================================================
+
+int CountWindowQueNum(int num[], int n)
+{
+    int i, cnt=0;
+    for (i=0; i<n; i++)
+        cnt+=num[i];
+    return cnt;
+}
+
+void fun2()
+{//只能过基本用例
+    int lineque[WINMAX];
+    int linenum[WINMAX]={0};
+    Person wait[PEOMAX];
+    int whead=0, wtail=0, wcnt=0;
+    int sttime[PEOMAX];//seconds
+    int vset[PEOMAX]={0}, vcnt=0;
+    int n,k;
+    int i,j, cur, mintime;
+    int h,m,s,p;
+    float sum=0;
+    int tmp;
+
+    for (i=0; i<PEOMAX; i++) {
+        sttime[i]=-1;
+    }
+
+    scanf("%d%d", &n,&k);
+    for (i=0; i<n; i++) {
+        scanf("%d:%d:%d %d", &h,&m,&s,&p);
+        if (h>=17&&(m>0||s>0)) continue;
+        wait[wcnt].hour=h;
+        wait[wcnt].min=m;
+        wait[wcnt].sec=s;
+        wait[wcnt].ptm=p>60?60*60:p*60;//>60?60:p;
+        wtail++;
+        wcnt++;
+    }
+    n=wcnt;
+    Sort(wait, wcnt);
+
+    //first inque,要考虑入队前人是否已经到了吗
+    for (i=0; i<n && i<k; i++) {
+        tmp=((wait[whead].hour-8)*3600+(wait[whead].min)*60+wait[whead].sec);
+        if (tmp>0) break;
+        lineque[i]=whead;
+        linenum[i]++;
+        whead++;
+        wcnt--;
+    }
+
+    //first set time
+    for (i=0; i<k; i++) {//windows
+        if (linenum[i]<1) continue;
+        cur=lineque[i];
+        if (sttime[cur]==-1)
+            sttime[cur]=0;
+    }
+
+    while (CountWindowQueNum(linenum, k) > 0) {
+        mintime=TIMEMAX;
+        for (i=0; i<k; i++) {//get mimtime
+            if (linenum[i]<1) continue;
+            cur=lineque[i];
+            mintime=(wait[cur].ptm+sttime[cur] < mintime)?(wait[cur].ptm+sttime[cur]):mintime;
+        }
+
+        for (i=0; i<k; i++) {//outque
+            if (linenum[i]<1) continue;
+            cur=lineque[i];
+            if (wait[cur].ptm+sttime[cur] == mintime) {
+                linenum[i]=0;
+            }
+        }
+
+        for (i=0; i<k; i++) {//windows,inque
+            if (wcnt<=0) break;//no people wait
+            tmp=((wait[whead].hour-8)*3600+(wait[whead].min)*60+wait[whead].sec);
+            if (tmp>mintime) break;
+            if (linenum[i]==0) {
+                lineque[i]=whead;
+                linenum[i]++;
+                whead++;
+                wcnt--;
+            }
+        }
+
+        //set time
+        for (i=0; i<k; i++) {//windows
+            if (linenum[i]==0) continue;
+            cur=lineque[i];
+            if (sttime[cur] == -1)
+                sttime[cur]=mintime;
+        }
+    }
+
+    for (i=0; i<n; i++) {
+        if (sttime[i]>9*60*60) continue;
+        tmp=((wait[i].hour-8)*3600+(wait[i].min)*60+wait[i].sec);
+        sum+=(sttime[i]-tmp)/60.0;
+        vcnt++;
+    }
+
+    if (vcnt==0)//好像没什么用
+        printf("0.0\n");
+    else {
+        sum=sum/vcnt;
+        printf("%.1f\n", sum);
+    }
+}
+//======================================================
+
+typedef struct stWait {
+    int come;
+    int ptm;
+} Wait;
+
+void Swap2(Wait per[], int x, int y)
+{
+    Wait p;
+    memcpy(&p, &per[x], sizeof(p));
+    memcpy(&per[x], &per[y], sizeof(p));
+    memcpy(&per[y], &p, sizeof(p));
+}
+
+void Sort2(Wait per[], int cnt)
+{
+    int i, j, min;
+    for (i=0; i<cnt-1; i++) {
+        min=i;
+        for (j=i+1; j<cnt; j++) {
+            if (per[j].come < per[min].come)
+                min=j;
+        }
+        Swap2(per, i, min);
+    }
+}
+
+void fun3()
+{
+    int window[WINMAX];
+    Wait wait[PEOMAX];
+    int wcnt=0;
+    int n,k;
+    int i,j, wmin;
+    int h,m,s,p;
+    float sum=0;
+
+    scanf("%d%d", &n,&k);
+    for (i=0; i<n; i++) {
+        scanf("%d:%d:%d %d", &h,&m,&s,&p);
+        if (h>=17&&(m>0||s>0)) continue;
+        wait[wcnt].come=h*60*60+m*60+s;
+        wait[wcnt].ptm=p>60?60*60:p*60;
+        wcnt++;
+    }
+
+    for (i=0; i<k; i++) {
+        window[i]=8*60*60;
+    }
+
+    Sort2(wait, wcnt);
+
+    for (i=0; i<wcnt; i++) {
+        wmin=0;
+        for (j=1; j<k; j++) {
+            wmin=window[j]<window[wmin]?j:wmin;
+        }
+
+        if (window[wmin]-wait[i].come > 0) {
+            sum+=window[wmin]-wait[i].come;
+            window[wmin]+=wait[i].ptm;
+        }
+        else {
+            //sum+=0;
+            window[wmin]=wait[i].come+wait[i].ptm;
+        }
+    }
+
+    if (wcnt==0)//好像没什么用
+        printf("0.0\n");
+    else {
+        sum=sum/60.0;
+        sum=sum/wcnt;
+        printf("%.1f\n", sum);
+    }
+
+}
+
+
 int main(int argc, char *argv[])
 {
-	fun1();
-
+    //fun1();
+    //fun2();
+    fun3();
 }
