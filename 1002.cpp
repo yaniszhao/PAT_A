53470001b8b92deffa26e7b0646d28f3818791e0
yaniszhao <361954576@qq.com>
2018-03-19 13:39:03 +0800

    modify 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -1,34 +1,13 @@
 #include <iostream>
+#include <strstream>//ostrstream
+#include <iomanip>//setprecision
 #include <cstdio>
 #include <string.h>
 #include <algorithm>
 using namespace std;
 #define MAX 1001
-void fun1()
-{
-    float map[MAX]={0};
-    int k,exp;
-    float coe;
-    string s, s1;
-    for (int i=0; i<2; i++) {
-        cin>>k;
-        for (int j=0; j<k; j++) {
-            cin>>exp>>coe;
-            map[exp]+=coe;
-        }
-    }
-    k=0;
-    for (int i=MAX-1; i>=0; i--)
-        if (map[i]!=0) {//要用!=不能用>因为可能小于0
-            k++;
-            s.append(" ").append(to_string(i)).append(" ");
-            s1=to_string(map[i]);
-            s+=s1.substr(0, s1.size()-5);
-        }
-    cout<<k<<s;
-}
 
-void fun2()
+void fun1()
 {
     float map[MAX]={0};
     int k,exp;
@@ -48,9 +27,58 @@ void fun2()
         if (map[i]!=0) printf(" %d %.1f", i, map[i]);
 }
 
-//fun1为了一次循环，多用了空间来存打印信息
-//fun2用两次循环，避免使用多的空间
+void fun2()
+{//不能四舍五入有个用例不过
+    float map[MAX]={0};
+    int k,exp;
+    float coe;
+    string s, s1;
+    for (int i=0; i<2; i++) {
+        cin>>k;
+        for (int j=0; j<k; j++) {
+            cin>>exp>>coe;
+            map[exp]+=coe;
+        }
+    }
+    k=0;
+    for (int i=MAX-1; i>=0; i--)
+        if (map[i]!=0) {//要用!=不能用>因为可能小于0
+            k++;
+            s.append(" ").append(to_string(i)).append(" ");
+            s1=to_string(map[i]);
+            //s+=s1.substr(0, s1.size()-5);
+            s+=s1.substr(0, s1.find('.')+2);
+        }
+    cout<<k<<s;
+}
+
+void fun3()
+{
+    float map[MAX]={0};
+    int k,exp;
+    float coe;
+    ostrstream s;
+    for (int i=0; i<2; i++) {
+        cin>>k;
+        for (int j=0; j<k; j++) {
+            cin>>exp>>coe;
+            map[exp]+=coe;
+        }
+    }
+    k=0;
+    for (int i=MAX-1; i>=0; i--)
+        if (map[i]!=0) {//要用!=不能用>因为可能小于0
+            k++;
+            s<<fixed<<setprecision(1);
+            s<<' '<<i<<' '<<map[i];
+        }
+    cout<<k<<s.str();
+}
+
+//fun1用两次循环，避免使用多的空间
+//fun2为了一次循环，多string来存打印信息,不能四舍五入有个用例不过
+//fun3为了一次循环，多strstream来存打印信息
 int main(int argc, char *argv[])
 {
-    fun1();
+    fun3();
 }
